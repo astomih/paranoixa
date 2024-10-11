557685ba275770482e53d8b46d7bd89223689617
astomih <astomih@users.noreply.github.com>
2024-10-11 04:42:02 +0000

    Deploying to gh-pages from @ astomih/paranoixa@33dbb6e4c37fedbd2a1aa5f4eb635fa385208cf5 🚀

---
+++ test/res/shader.frag.wgsl
@@ -0,0 +1,32 @@
+struct FragmentOutput {
+    @location(0) outColor: vec4<f32>,
+}
+
+var<private> inColor_1: vec3<f32>;
+var<private> inUV_1: vec2<f32>;
+var<private> outColor: vec4<f32>;
+@group(0) @binding(0) 
+var tex: texture_2d<f32>;
+@group(0) @binding(1) 
+var samp: sampler;
+
+fn main_1() {
+    var f: vec2<f32>;
+
+    let _e5 = inUV_1;
+    let _e8 = inUV_1;
+    f = vec2<f32>(_e5.x, (1f - _e8.y));
+    let _e14 = f;
+    let _e15 = textureSample(tex, samp, _e14);
+    outColor = _e15;
+    return;
+}
+
+@fragment 
+fn main(@location(0) inColor: vec3<f32>, @location(1) inUV: vec2<f32>) -> FragmentOutput {
+    inColor_1 = inColor;
+    inUV_1 = inUV;
+    main_1();
+    let _e15 = outColor;
+    return FragmentOutput(_e15);
+}
---
+++ test/test.cpp
@@ -0,0 +1,58 @@
+#include <paranoixa/allocator/tlsf_allocator.hpp>
+#include <paranoixa/paranoixa.hpp>
+
+#include <memory>
+void MemoryAllocatorTest();
+void PtrTest();
+int main() {
+
+  using namespace paranoixa;
+  MemoryAllocatorTest();
+  PtrTest();
+  auto allocator = MakeAllocatorPtr<TLSFAllocator>(0x2000);
+  StdAllocator<int> stdAllocator{allocator};
+  std::vector<int, StdAllocator<int>> vec({allocator});
+  vec.push_back(1);
+  {
+    auto app = Paranoixa({.allocator = allocator, .api = GraphicsAPI::WebGPU});
+    app.Run();
+  }
+  return 0;
+}
+
+void MemoryAllocatorTest() {
+  using namespace paranoixa;
+  std::cout << "---------------MemoryAllocatorTest------------" << std::endl;
+  Allocator *allocator = new TLSFAllocator(0x2000);
+  void *ptr = allocator->Allocate(128);
+  allocator->Free(ptr, 128);
+  delete allocator;
+  std::cout << "----------------------------------------------" << std::endl;
+}
+
+void PtrTest() {
+  struct A {
+    int a;
+  };
+  struct B : public A {
+    B(int a) : A{a}, b(0) {}
+    int b;
+  };
+  using namespace paranoixa;
+  std::cout << "---------------PtrTest------------" << std::endl;
+  {
+    auto allocator = MakeAllocatorPtr<TLSFAllocator>(0x2000);
+    {
+      Ptr<A> ptr{allocator};
+      ptr = MakePtr<B>(allocator, 10);
+      std::cout << ptr->a << std::endl;
+      ptr.Reset();
+    }
+    {
+      UniquePtr<A> ptr{allocator};
+      ptr = MakeUnique<B>(allocator, 10);
+    }
+    std::cout << "allocator.count : " << allocator.use_count() << std::endl;
+  }
+  std::cout << "---------------------------------" << std::endl;
+}
